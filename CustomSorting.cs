14ff16051cecc06024f1746c0baf19758bdec95f
smalwaysurs98 <rsrahulsin9852@gmail.com>
2026-02-03 14:51:20 +0530

    New Assignment file added

--- CustomSorting.cs
+++ CustomSorting.cs
@@ -1,37 +1,37 @@
-using System;
+// using System;
 
-public class Student
-{
-    public string Name{get;set;}
-    public int Age{get;set;}
-    public int Marks{get;set;}
-}
-public class MarksComparer : IComparer<Student>
-{
-    public int Compare(Student s1,Student s2)
-    {
-        int res = s2.Marks.CompareTo(s1.Marks);
-        if(res == 0)
-        {
-            return s1.Age.CompareTo(s1.Age);
-        }
-        return res;
-    }
-}
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<Student> st = new List<Student>
-        {
-            new Student(){Name ="Sachin",Age = 24,Marks = 36},
-            new Student(){Name ="Mohan",Age = 23,Marks = 32},
-            new Student(){Name ="Rahul",Age = 35,Marks = 45},
-        };
-        st.Sort(new MarksComparer());
-        foreach(var it in st)
-        {
-            Console.WriteLine($"{it.Name} | {it.Age} | {it.Marks}");
-        }
-    }
-}
+// public class Student
+// {
+//     public string Name{get;set;}
+//     public int Age{get;set;}
+//     public int Marks{get;set;}
+// }
+// public class MarksComparer : IComparer<Student>
+// {
+//     public int Compare(Student s1,Student s2)
+//     {
+//         int res = s2.Marks.CompareTo(s1.Marks);
+//         if(res == 0)
+//         {
+//             return s1.Age.CompareTo(s1.Age);
+//         }
+//         return res;
+//     }
+// }
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         List<Student> st = new List<Student>
+//         {
+//             new Student(){Name ="Sachin",Age = 24,Marks = 36},
+//             new Student(){Name ="Mohan",Age = 23,Marks = 32},
+//             new Student(){Name ="Rahul",Age = 35,Marks = 45},
+//         };
+//         st.Sort(new MarksComparer());
+//         foreach(var it in st)
+//         {
+//             Console.WriteLine($"{it.Name} | {it.Age} | {it.Marks}");
+//         }
+//     }
+// }
--- MultiplicationTable.cs
+++ MultiplicationTable.cs
@@ -1,25 +1,25 @@
-using System;
-using System.Reflection.Metadata;
-using Microsoft.VisualBasic;
-class MultiplicationTable
-{
-    public static void Main(string[] args)
-    {
-        int n;
-        int upto;
-        Console.WriteLine("Enter the number: ");
-        n = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the limit upto which the multiples are required: ");
-        upto = Convert.ToInt32(Console.ReadLine());
+// using System;
+// using System.Reflection.Metadata;
+// using Microsoft.VisualBasic;
+// class MultiplicationTable
+// {
+//     public static void Main(string[] args)
+//     {
+//         int n;
+//         int upto;
+//         Console.WriteLine("Enter the number: ");
+//         n = Convert.ToInt32(Console.ReadLine());
+//         Console.WriteLine("Enter the limit upto which the multiples are required: ");
+//         upto = Convert.ToInt32(Console.ReadLine());
 
-        int[] MultiplicationArray = new int[upto];
-        for(int i = 0; i<upto; i++)
-        {
-            MultiplicationArray[i] = n*(i+1);
-        }
-        for(int i = 0; i < upto; i++)
-        {
-            Console.Write(MultiplicationArray[i]);
-        }
-    }
-}
+//         int[] MultiplicationArray = new int[upto];
+//         for(int i = 0; i<upto; i++)
+//         {
+//             MultiplicationArray[i] = n*(i+1);
+//         }
+//         for(int i = 0; i < upto; i++)
+//         {
+//             Console.Write(MultiplicationArray[i]);
+//         }
+//     }
+// }
